5ac10a8b0cbd66410bfe8552cf2c55632e60ed24
Shamil <ishamil86@gmail.com>
2020-10-29 14:44:42 +0300

    added ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "libft.h"
+
+size_t strlcat(char * restrict dst, const char * restrict src, size_t dsrsize)
+{
+	size_t	i;
+	size_t	len;
+
+	len = ft_strlen(dst) + ft_strlen(src);
+	if (size <= ft_strlen(dst))
+		return (ft_strlen(src) + size);
+	while (*dst)
+		dst++;
+	i = 0;
+	while ((i < size - (len - ft_str(src)) - 1) && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (len);
+}
