3c186c80736718d4fb77006e1c8f677c182d27b1
Shamil <54128722+ShamilSE@users.noreply.github.com>
2021-03-19 10:46:14 +0300

    Update ft_tolower.c
    fixed length of chars to copy

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,18 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mismene <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/30 11:57:33 by mismene           #+#    #+#             */
-/*   Updated: 2020/10/30 11:57:40 by mismene          ###   ########.fr       */
+/*   Created: 2020/12/04 20:39:22 by mismene           #+#    #+#             */
+/*   Updated: 2021/03/19 10:45:07 by mismene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_tolower(int c)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-    return ((c >= 65 && c < 91) ? (c + 32) : (c));
+	char	*d;
+	char	*s;
+	size_t	n;
+	size_t	i;
+
+	i = 0;
+	n = dstsize;
+	d = dst;
+	s = (char *)src;
+	if (dst == NULL || src == NULL)
+		return (0);
+	if (n != 0)
+	{
+		while (src[i] && i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	printf("copied %zu chars\n", ft_strlen(dst));
+	return (ft_strlen(dst));
 }
